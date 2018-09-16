588ee18d80b5c1013fde1cefe309b24f017d27d1
Hideaki Murakami <thehiderofkeys@gmail.com>
2018-09-16 19:47:57 +1200

    Added Text Field

--- ms-sub/src/App.tsx
+++ ms-sub/src/App.tsx
@@ -1,22 +1,69 @@
+import { Button } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from '@material-ui/core/TextField';
 import * as React from 'react';
 import './App.css';
+interface IState{
+  button: any,
+  query: any,
+  result: any
+  textfield: any
+}
 
-import logo from './logo.svg';
-
-class App extends React.Component {
+export default class App extends React.Component<{},IState> {
+  constructor(props: any){
+    super(props);
+    this.state = {
+      button: this.buttonPressed.bind(this),
+      query: "",
+      result: "",
+      textfield: this.onText.bind(this)
+    }
+  }
+  public onText(q: any) {
+    this.setState({
+      query: q.value,
+    });
+  }
+  public buttonPressed(){
+    this.setState({
+      result: " ",
+    });
+    this.search(this.state.query)
+  }
+  public search(patientToSearch: any){
+    fetch('api.flutrack.org/?s=headache')
+    .then(response => response.json()).then(data => {
+      this.setState({result: data})
+    })
+  }
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="container-fluid">
+        <div className="centreText">
+          <div className="dropZone">
+          <TextField
+          id="name"
+          label="Search"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          placeholder="eg.Coughing"
+          value={this.state.query}
+          onChange={this.state.textfield}
+          margin="normal"
+          />
+          <Button onClick={this.state.button}>Search</Button>
+          </div>
+          <div  className="dank">
+          {
+            this.state.result === " " && !(this.state.query === "") ?
+            <CircularProgress thickness={3} />:
+            <p>{this.state.result}</p>
+          }
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-export default App;
